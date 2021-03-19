ec4f344e9482c012168a17893472753e12719135
Thegoodspirit <80534369+Thegoodspirit@users.noreply.github.com>
2021-03-19 22:15:22 +0530

    Update dynamic_array_implimentation.py

--- dynamic_array_implimentation.py
+++ dynamic_array_implimentation.py
@@ -3,13 +3,16 @@ import ctypes
 class DynamicArray():
 
     def __init__(self):
-        self.n = 0
-        self.capacity = 1
-        self.A = self.make_array(self.capacity)
+        self.n = 0  # default index where the element wass insert 
+        self.capacity = 1 # capacity of store the element
+        self.A = self.make_array(self.capacity) 
 
     def __len__(self):
         return self.n
 
+    """  They are implemented by __getitem__() and __setitem__() methods. But, these methods are used only in indexed attributes like 
+    arrays dictionaries, lists e.t.c. """
+    
     def __getitem__(self,k):
 
         if not 0<= k < self.n:
@@ -17,15 +20,15 @@ class DynamicArray():
 
         return self.A[k]
 
-    def append(self,ele):
+    def append(self,ele): # This function is To add the element in array
         
         if self.n == self.capacity:
             self._resize(2*self.capacity)
 
         self.A[self.n]=ele
         self.n +=1
-
-    def _resize(self,new_cap):
+# this symbol was ( _ ) denote that this 'resize' is a private function
+    def _resize(self,new_cap):  # This fuction is To resize the array automatically when index of element and capacity is equal 
         B = self.make_array(new_cap)
 
         for k in range(self.n):
