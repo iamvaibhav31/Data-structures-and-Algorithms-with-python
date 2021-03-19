1bfd3bcfc3fd5f613a0e9a10243e493a02f1269b
Thegoodspirit <80534369+Thegoodspirit@users.noreply.github.com>
2021-03-19 21:55:46 +0530

    Add files via upload

---
+++ dynamic_array_implimentation.py
@@ -0,0 +1,51 @@
+import ctypes
+
+class DynamicArray():
+
+    def __init__(self):
+        self.n = 0
+        self.capacity = 1
+        self.A = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self,k):
+
+        if not 0<= k < self.n:
+            return IndexError("_!_K IS OUT OF BOUND_!_")
+
+        return self.A[k]
+
+    def append(self,ele):
+        
+        if self.n == self.capacity:
+            self._resize(2*self.capacity)
+
+        self.A[self.n]=ele
+        self.n +=1
+
+    def _resize(self,new_cap):
+        B = self.make_array(new_cap)
+
+        for k in range(self.n):
+            B[k] = self.A[k]
+
+        self.A = B
+        self.capacity = new_cap
+
+    def make_array(self , new_cap):
+        return (new_cap*ctypes.py_object)()
+
+
+if __name__ == "__main__":
+
+    arr = DynamicArray()
+
+    arr.append(21)
+    print(len(arr))
+    arr.append(31)
+    print(len(arr))
+    print(arr[0])
+    print(arr[1])
+    print(arr[2])
